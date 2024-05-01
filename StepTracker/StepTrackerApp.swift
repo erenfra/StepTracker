6652ef1f4fdd3c06460343d93f11f72726327b4b
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-05-01 17:22:51 -0500

    Initial Commit

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Renato Oliveira Fraga on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
