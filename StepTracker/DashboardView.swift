e496f227257d55aa62ecf34802b738354cb527fa
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-05-03 00:01:02 -0500

    navigation metric completed

--- StepTracker/DashboardView.swift
+++ StepTracker/DashboardView.swift
@@ -7,18 +7,82 @@
 
 import SwiftUI
 
-struct DashboardView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+enum HealthMetricContext: CaseIterable, Identifiable {
+  case steps, weight
+  var id: Self { self }
+  var title: String {
+    switch self {
+    case .steps:
+      return "Steps"
+    case .weight:
+      return "Weight"
     }
+  }
 }
 
+struct DashboardView: View {
+  @State private var selectedStat: HealthMetricContext = .steps
+  var isSteps: Bool { selectedStat == .steps }
+  var body: some View {
+    NavigationStack {
+      ScrollView {
+        VStack(spacing: 20) {
+          Picker("Selected Stats", selection: $selectedStat) {
+            ForEach(HealthMetricContext.allCases) { metric in
+              Text(metric.title)
+            }
+          }.pickerStyle(.segmented)
+          VStack {
+            NavigationLink(value: selectedStat) {
+              HStack {
+                VStack(alignment: .leading) {
+                  Label("Steps", systemImage: "figure.walk")
+                    .font(.title3.bold())
+                    .foregroundStyle(.pink)
+                  Text("Avg: 10K Steps")
+                    .font(.caption)
+
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+
+              }
+            }
+              .foregroundStyle(.secondary)
+              .padding(.bottom, 12)
+            RoundedRectangle(cornerRadius: 12)
+              .foregroundStyle(.secondary)
+              .frame(height: 150)
+          }
+          .padding()
+          .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+          VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+              Label("Averages", systemImage: "calendar")
+                .font(.title3.bold())
+                .foregroundStyle(.pink)
+              Text("Last 28 Days")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+            }
+            .padding(.bottom, 12)
+            RoundedRectangle(cornerRadius: 12)
+              .foregroundStyle(.secondary)
+              .frame(height: 240)
+          }
+          .padding()
+          .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+        }
+      }
+      .padding()
+        .navigationTitle("Dashboard")
+        .navigationDestination(for: HealthMetricContext.self) { metric in
+          Text(metric.title)
+        }
+    }
+    .tint(isSteps ? .pink : .indigo)
+  }
+}
 #Preview {
-    DashboardView()
+  DashboardView()
 }
